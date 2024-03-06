aa9fc863d66e8207283fac4c74acf9daead47de2
bercivarga <65171545+bercivarga@users.noreply.github.com>
2024-03-06 21:01:22 +0100

    add tailwind configurations for fonts and base element styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +10,66 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        display: ["var(--font-display)", "sans-serif"],
+        sans: ["var(--font-sans)", "sans-serif"],
+        body: ["var(--font-sans)", "sans-serif"],
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          lg: "1.5rem",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, addComponents, theme }) {
+      addComponents({});
+      addBase({
+        h1: {
+          fontSize: theme("fontSize.5xl"),
+          fontFamily: theme("fontFamily.display"),
+          lineHeight: theme("lineHeight.tight"),
+          "@screen lg": {
+            fontSize: theme("fontSize.7xl"),
+          },
+        },
+        h2: {
+          fontSize: theme("fontSize.3xl"),
+          fontFamily: theme("fontFamily.display"),
+          "@screen lg": {
+            fontSize: theme("fontSize.5xl"),
+          },
+        },
+        h3: {
+          fontSize: theme("fontSize.2xl"),
+          fontFamily: theme("fontFamily.display"),
+          "@screen lg": {
+            fontSize: theme("fontSize.4xl"),
+          },
+        },
+        h4: {
+          fontSize: theme("fontSize.xl"),
+          fontFamily: theme("fontFamily.display"),
+          "@screen lg": {
+            fontSize: theme("fontSize.2xl"),
+          },
+        },
+        h5: {
+          fontSize: theme("fontSize.lg"),
+        },
+        h6: {
+          fontSize: theme("fontSize.base"),
+        },
+        body: {
+          "font-family": theme("fontFamily.sans"),
+          "font-size": theme("fontSize.base"),
+          "font-weight": theme("fontWeight.normal"),
+        },
+      });
+    }),
+  ],
 };
 export default config;
