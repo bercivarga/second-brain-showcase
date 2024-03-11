4b01ec611b961c8bcbf5a018d563c8badea77323
bercivarga <65171545+bercivarga@users.noreply.github.com>
2024-03-11 21:38:47 +0100

    add metadata files to public folder

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
-  title: "Second Brain Showcase Project",
+  title: "Second Brain | Home",
   description: "This is a showcase portfolio project. Made by @berci.dev",
 };
 
@@ -16,10 +16,14 @@ export default function Home() {
     <main className="flex min-h-screen items-center justify-center">
       <div className="flex flex-col gap-4 p-6 md:items-center md:text-center">
         <h2>
-          Welcome to the
-          <br /> CF Showcase
+          Welcome to your
+          <br /> Second Brain
         </h2>
-        <p>Sign in or sign up in order to explore the app.</p>
+        <p>
+          {userId
+            ? "Click on the button to proceed to the platform."
+            : "Sign in or sign up in order to explore the app."}
+        </p>
         <div className="flex gap-2">
           {userId ? (
             <Link href="/notes">
---
+++ app/base-head.tsx
@@ -0,0 +1,27 @@
+export default function BaseHead() {
+  return (
+    <head>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#030303" />
+      <meta name="msapplication-TileColor" content="#d1e4ff" />
+      <meta name="theme-color" content="#d1e4ff" />
+    </head>
+  );
+}
